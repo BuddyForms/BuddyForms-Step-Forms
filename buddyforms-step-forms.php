5064bc42208f5112a2c3cc46adf94cae409fb916
jnfDev <jairo.jnf.2010@gmail.com>
2020-07-14 15:36:20 -0400

    improvement: - Separating enqueue scripts process between Front and Admin. - Separating the main JavaScript file into Front and Admin. - Separating the main CSS file into Front and Admin. - Load SmartWizard JQuery library on the front.

--- buddyforms-step-forms.php
+++ buddyforms-step-forms.php
@@ -60,11 +60,11 @@ function buddyforms_step_forms_load_plugin_textdomain() {
 
 // function to enqueue stylesheets and scripts
 
-function buddyforms_step_forms_scripts() {
+function buddyforms_step_forms_admin_scripts() {
 
 	wp_enqueue_style( 'roboto-google-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap' );
 	wp_enqueue_style( 'tree-css', BUDDYFORMS_STEP_FORMS_ASSETS . '/resources/jqtree/jqtree.css' );
-	wp_enqueue_style( 'step-forms.css', BUDDYFORMS_STEP_FORMS_ASSETS . '/css/buddyforms.step.forms.css' );
+	wp_enqueue_style( 'step-forms.css', BUDDYFORMS_STEP_FORMS_ASSETS . '/css/buddyforms.step.forms-admin.css' );
 
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_script( 'jquery-ui-core' );
@@ -82,9 +82,26 @@ function buddyforms_step_forms_scripts() {
 	wp_register_script( 'tree-js', BUDDYFORMS_STEP_FORMS_ASSETS . '/resources/jqtree/tree.jquery.js' , array( 'jquery' ) );
 	wp_enqueue_script( 'tree-js' );
 
-	wp_register_script( 'step-forms-js', BUDDYFORMS_STEP_FORMS_ASSETS . '/js/buddyforms.step.forms.js' , array( 'jquery' ) );
+	wp_register_script( 'step-forms-js', BUDDYFORMS_STEP_FORMS_ASSETS . '/js/buddyforms.step.forms-admin.js' , array( 'jquery' ) );
 	wp_enqueue_script( 'step-forms-js' );
 
 }
 
-add_action( 'admin_enqueue_scripts', 'buddyforms_step_forms_scripts' );
+add_action( 'admin_enqueue_scripts', 'buddyforms_step_forms_admin_scripts' );
+
+
+function buddyforms_step_forms_front_scripts() {
+
+	wp_enqueue_style( 'smartwizard-css', BUDDYFORMS_STEP_FORMS_ASSETS . '/resources/smartwizard/smart_wizard_all.css' );
+	wp_enqueue_style( 'step-forms.css', BUDDYFORMS_STEP_FORMS_ASSETS . '/css/buddyforms.step.forms-front.css' );
+
+
+	wp_register_script( 'smartwizard-js', BUDDYFORMS_STEP_FORMS_ASSETS . '/resources/smartwizard/jquery.smartWizard.js', array( 'jquery' ) );
+	wp_enqueue_script( 'smartwizard-js' );
+
+	wp_register_script( 'step-forms-js', BUDDYFORMS_STEP_FORMS_ASSETS . '/js/buddyforms.step.forms-front.js' , array( 'jquery' ) );
+	wp_enqueue_script( 'step-forms-js' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'buddyforms_step_forms_front_scripts' );
